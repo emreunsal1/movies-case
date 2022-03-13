3bc16681779e0c1731cd70609e592c01b8a0be63
Emre Ünsal <unsalemre111@gmail.com>
2022-03-13 23:41:05 +0300

    add movie service axios instance

--- src/utils/movie-service.ts
+++ src/utils/movie-service.ts
@@ -1,18 +1,20 @@
 import axios from "axios";
 import { IMovie } from "./../interfaces";
 
+const movieService = axios.create({
+  params: {
+    apikey: process.env.NEXT_PUBLIC_API_KEY,
+  },
+});
+
 export const searchMovies = async (keyword: string): Promise<IMovie[]> => {
   try {
-    const response = await axios.get(`http://www.omdbapi.com/`, {
+    const response = await movieService.get(`https://www.omdbapi.com/`, {
       params: {
         s: keyword,
-        apikey: process.env.NEXT_PUBLIC_API_KEY,
       },
     });
-    if (response.data.Search) {
-      return response.data.Search;
-    }
-    return [];
+    return response.data.Search || [];
   } catch (e) {
     return [];
   }
