a896fccb13678e80746cf25c0fe85f12f84d5625
Emre Ünsal <unsalemre111@gmail.com>
2022-03-13 22:22:23 +0300

    update file structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { fetchMovies } from "../utils/movie-service";
+import { IMovie } from "./../interfaces";
+
+const Home: NextPage = () => {
+  const [keyword, setKeyword] = useState("");
+  const [movies, setMovies] = useState<IMovie[]>([]);
+
+  const searchOnClick = async () => {
+    const response = await fetchMovies(keyword);
+    console.log(response);
+    setMovies(response);
+  };
+  return (
+    <div>
+      <input placeholder="search" onChange={(e) => setKeyword(e.target.value)}></input>
+      <button onClick={searchOnClick}>Search</button>
+      <ul>
+        {movies.map((movie) => {
+          return <li key={movie.imdbID}>{movie.Title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
