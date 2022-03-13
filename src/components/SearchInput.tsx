beeb781ed846ad5a13b827bb16736e76b4e29267
Emre Ünsal <unsalemre111@gmail.com>
2022-03-14 00:28:20 +0300

    add search input components

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Image from "next/image";
+
+interface ISearchInputProps {
+  placeholder: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  state: string;
+}
+
+export default function SearchInput({ placeholder, onChange, state }: ISearchInputProps) {
+  return (
+    <div>
+      <Image src="/icons/search-icon.svg" alt="searchIcon" height={18.75} width={18.75} />
+      <input type="text" value={state} onChange={onChange} placeholder={placeholder} />
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,21 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 import Button from "../components/Button";
-import { searchMovies } from "../utils/movie-service";
+import { searchMoviesClient } from "../utils/movie-service";
 import { IMovie } from "./../interfaces";
+import SearchInput from "../components/SearchInput";
 
 const Home: NextPage = () => {
   const [keyword, setKeyword] = useState("");
   const [movies, setMovies] = useState<IMovie[]>([]);
 
   const searchOnClick = async () => {
-    const response = await searchMovies(keyword);
+    const response = await searchMoviesClient(keyword);
     setMovies(response);
   };
   return (
     <div>
-      <input placeholder="search" onChange={(e) => setKeyword(e.target.value)}></input>
+      <SearchInput state={keyword} onChange={(e) => setKeyword(e.target.value)} placeholder="Search" />
       <Button onClick={searchOnClick}>Search</Button>
       <ul>
         {movies.map((movie) => (
