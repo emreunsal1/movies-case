a9986bdd49ee082af408df28dc15455d9b68e44f
Emre Ünsal <unsalemre111@gmail.com>
2022-03-14 01:54:33 +0300

    change file and class name

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -9,7 +9,7 @@ interface ISearchInputProps {
 
 export default function SearchInput({ placeholder, onChange, state }: ISearchInputProps) {
   return (
-    <div>
+    <div className="m-search-bar">
       <Image src="/icons/search-icon.svg" alt="searchIcon" height={18.75} width={18.75} />
       <input type="text" value={state} onChange={onChange} placeholder={placeholder} />
     </div>
