90108a4f2b6a58138e00a6126023e7f1fdd73ca1
Emre Ünsal <unsalemre111@gmail.com>
2022-03-14 19:54:16 +0300

    add search focus style

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
 interface ISearchInputProps {
@@ -8,10 +8,18 @@ interface ISearchInputProps {
 }
 
 export default function SearchInput({ placeholder, onChange, state }: ISearchInputProps) {
+  const [isFocus, setIsFocus] = useState(false);
   return (
-    <div className="m-search-bar">
+    <div className={`m-search-bar ${isFocus ? "focus" : ""}`}>
       <Image src="/icons/search-icon.svg" alt="searchIcon" height={18.75} width={18.75} />
-      <input type="text" value={state} onChange={onChange} placeholder={placeholder} />
+      <input
+        type="text"
+        value={state}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
     </div>
   );
 }
